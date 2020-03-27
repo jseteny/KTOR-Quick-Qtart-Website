af9d090c9ab5d81bc2476471e37ad226dc64826d
Setény János <seteny.janos@tigra.hu>
2020-03-27 22:34:17 +0100

    html-dsl

--- src/Application.kt
+++ src/Application.kt
@@ -6,17 +6,15 @@ import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.auth.*
 import io.ktor.features.PartialContent
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
-import io.ktor.http.ContentType
+import io.ktor.html.respondHtml
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
 import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
-import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
@@ -24,7 +22,7 @@ import io.ktor.routing.routing
 import io.ktor.sessions.Sessions
 import io.ktor.sessions.cookie
 import io.ktor.sessions.sessions
-import javax.print.attribute.standard.JobOriginatingUserName
+import kotlinx.html.*
 
 // Use "gradle -t installDist" to have automatic recompile
 // See https://ktor.io/servers/autoreload.html#recompiling-automatically-on-source-changes
@@ -72,7 +70,7 @@ fun Application.module(testing: Boolean = false) {
                 if (userName != null && userName == post["password"]) {
                     call.sessions.set("SESSION", MySession(userName))
                     //val principal = call.principal<UserIdPrincipal>() ?: error("No principal")
-                    call.respondRedirect("/html-freemarker", permanent = false)
+                    call.respondRedirect("/html-dsl", permanent = false)
                 } else {
                     call.respond(FreeMarkerContent("login.ftl", mapOf("error" to "Error during login")))
                 }
@@ -92,14 +90,26 @@ fun Application.module(testing: Boolean = false) {
             // call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
         }
 
-        get("/html-freemarker") {
+
+        get("/html-dsl") {
+
             val session = call.sessions.get("SESSION") as MySession
-            call.respond(FreeMarkerContent("index.ftl", etag = "",
-                    model = mapOf(
-                            "data" to IndexData(listOf(1, 2, 3)),
-                            "userName" to session.userName
-                    )
-            ))
+            val data = IndexData(listOf(1, 2, 3))
+            call.respondHtml {
+                head {
+                    link(rel = "stylesheet", href = "static/styles.css")
+                }
+                body {
+                    p {
+                        text("Username: ${session.userName}")
+                    }
+                    ul {
+                        for (item in data.items) {
+                            li { +"$item" }
+                        }
+                    }
+                }
+            }
         }
     }
 }
