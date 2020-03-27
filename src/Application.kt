5f4ffb53710b7eda149a31d54bc486b215c59f0c
Setény János <seteny.janos@tigra.hu>
2020-03-27 21:56:26 +0100

    todo https://ktor.io/quickstart/guides/website.html#sessions

--- src/Application.kt
+++ src/Application.kt
@@ -6,24 +6,20 @@ import freemarker.cache.ClassTemplateLoader
 import io.ktor.application.Application
 import io.ktor.application.call
 import io.ktor.application.install
-import io.ktor.auth.Authentication
-import io.ktor.auth.UnauthorizedResponse
-import io.ktor.auth.UserIdPrincipal
-import io.ktor.auth.form
 import io.ktor.features.PartialContent
 import io.ktor.freemarker.FreeMarker
 import io.ktor.freemarker.FreeMarkerContent
-import io.ktor.http.ContentType
 import io.ktor.http.content.resources
 import io.ktor.http.content.static
 import io.ktor.request.receiveParameters
 import io.ktor.response.respond
 import io.ktor.response.respondRedirect
-import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.post
 import io.ktor.routing.route
 import io.ktor.routing.routing
+import io.ktor.sessions.Sessions
+import io.ktor.sessions.cookie
 
 // Use "gradle -t installDist" to have automatic recompile
 // See https://ktor.io/servers/autoreload.html#recompiling-automatically-on-source-changes
@@ -41,6 +37,13 @@ fun Application.module(testing: Boolean = false) {
     install(FreeMarker) {
         templateLoader = ClassTemplateLoader(this::class.java.classLoader, "templates")
     }
+
+
+    // todo https://ktor.io/quickstart/guides/website.html#sessions
+
+
+/* Did not work for me. See  https://ktor.io/quickstart/guides/website.html#using-the-form-authentication
+
     install(Authentication) {
         form("login") {
             userParamName = "username"
@@ -49,6 +52,7 @@ fun Application.module(testing: Boolean = false) {
             validate { credentials -> if (credentials.name == credentials.password) UserIdPrincipal(credentials.name) else null }
         }
     }
+*/
 
     routing {
         static("/static") {
@@ -66,7 +70,8 @@ fun Application.module(testing: Boolean = false) {
                     call.respond(FreeMarkerContent("login.ftl", mapOf("error" to "Error during login")))
                 }
             }
-/*
+/* Did not work for me. See https://ktor.io/quickstart/guides/website.html#using-the-form-authentication
+
             authenticate("login") {
                 post {
                     val principal = call.principal<UserIdPrincipal>()
